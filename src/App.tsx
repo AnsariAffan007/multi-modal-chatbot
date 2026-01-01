07d542f7e95111e36aaea0d409b4a4b9fb5775bc
AnsariAffan007 <ansariaffan014@gmail.com>
2026-01-01 12:05:26 +0530

    add simplebar scroll to messages area

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./app.css"
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 function App() {
 
@@ -10,32 +12,36 @@ function App() {
           <p>Multi-modal Chatbot</p>
         </div>
         <div className="chatarea">
-          <div className="message sent">Hello</div>
-          <div className="message recieved">Hi, How can I help you?</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
-          <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+          <SimpleBar style={{ height: "100%" }}>
+            <div className="chats-container">
+              <div className="message sent">Hello</div>
+              <div className="message recieved">Hi, How can I help you?</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message sent">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+              <div className="message recieved">This is a long message just to test the UI if it overflows, gets wrapped, or the max width takes effect or not</div>
+            </div>
+          </SimpleBar>
         </div>
         <div className="chatinput">
           <input name="chat-prompt" type="text" placeholder="Type your prompt" />
