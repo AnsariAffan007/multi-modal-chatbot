105b15a07035c72c053653516ebeb1ffc221fda3
AnsariAffan007 <ansariaffan014@gmail.com>
2026-01-01 12:44:28 +0530

    make call to local ollama using openai client sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { useRef, useState, type FormEvent } from "react";
 import "./app.css"
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
+import OpenAI from "openai";
+
+const openaiClient = new OpenAI({
+  baseURL: "http://localhost:11434/v1/",
+  apiKey: "ollama",
+  dangerouslyAllowBrowser: true
+});
 
 function App() {
+
+  // const [messages, setMessages] = useState<{ party: "ai" | "self", message: string }[]>([])
+
+  const [_loading, setLoading] = useState(false)
+  const promptInputRef = useRef<HTMLInputElement>(null)
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const prompt = new FormData(e.currentTarget).get('chat-prompt') as string
+    if (!prompt || prompt === '') return;
+    setLoading(true)
+    const response = await openaiClient.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant" },
+        { role: "user", content: prompt }
+      ],
+      model: "gemma3:1b"
+    })
+    const responseMessage = response.choices[0].message.content
+    console.log(responseMessage)
+    if (promptInputRef.current) promptInputRef.current.value = ""
+    setLoading(false)
+  }
 
   return (
     <div className="container">
@@ -43,10 +73,10 @@ function App() {
             </div>
           </SimpleBar>
         </div>
-        <div className="chatinput">
-          <input name="chat-prompt" type="text" placeholder="Type your prompt" />
-          <button>Send {" >"}</button>
-        </div>
+        <form onSubmit={sendMessage} className="chatinput">
+          <input name="chat-prompt" type="text" placeholder="Type your prompt" ref={promptInputRef} />
+          <button type="submit">Send {" >"}</button>
+        </form>
       </div>
 
     </div>
